b95c61be2f0c13de6c014de2d3ee237a830adc6d
Rebecca Kibisu <coursecpd@gmail.com>
2025-06-14 23:57:22 +0300

    variables2

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code in this file!
+const currentUser = 'Vugutsa'
+const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!"
+const upperCaseCurrentUser = "VUGUTSA"
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + upperCaseCurrentUser + "!"
+const firstInitial = "V"
+const restOfName = "ugutsa"
+let shortGreeting = "Welcome, " + firstInitial +"!"
